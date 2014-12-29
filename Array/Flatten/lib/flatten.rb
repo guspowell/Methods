b6c6f4b133132122bf7f1b7cfecc8baad3074739
guspowell <augustuspowell@me.com>
2014-12-29 17:29:14 +0000

    can't do flattengit add *

--- Array/Flatten/lib/flatten.rb
+++ Array/Flatten/lib/flatten.rb
@@ -1,12 +1,15 @@
 class Array
 
-	def my_flatten
-		self.each do |x|
-			if x.class == Array
-				x.each { |y| self << y }
-				self.delete(x)
-			end
-		end
-	end
+# self = [1,2,3,[[4,5],6]]
+
+	# def my_flatten
+	# 	self.each do |x|
+	# 		if x.class == Array
+	# 			x.each { |y| self << y }
+	# 			self.delete(x)
+	# 		end
+	# 	end
+	# end
+
 
 end
--- Array/Flatten/spec/flatten_spec.rb
+++ Array/Flatten/spec/flatten_spec.rb
@@ -8,4 +8,12 @@ describe Array do
 		expect(['dog','cat',3,[4,:bear]].my_flatten).to eq ['dog','cat',3, 4, :bear]
 	end
 
+	it 'should reduce an array within an array within an array' do
+		expect([1,2,3,[[4,5],6]].my_flatten).to eq [1,2,3,4,5,6]
+	end
+
+	# it 'should reduce an array within an array within an array' do
+	# 	expect([1,2,3,[4,[[5,6],7]]].my_flatten).to eq [1,2,3,4,5,6,7]
+	# end
+
 end
