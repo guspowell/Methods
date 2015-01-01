28bb402b2b4ff35f3730932ca51d9416c014e008
guspowell <augustuspowell@me.com>
2015-01-01 17:49:14 +0000

    can't do symbol arguments

--- Array/Inject/lib/inject.rb
+++ Array/Inject/lib/inject.rb
@@ -2,7 +2,7 @@ class Array
 
 	# [1,2,3,4].inject {|memo,item| memo + item}
 
-	def my_inject(memo = self[0])
+	def my_inject(memo = self[0], sym = nil)
 		copy = self.dup																# copy = [1,2,3,4]
 		copy.shift if memo == self[0]									# copy = [2,3,4]
 		copy.each do |item|														# going through each of copy
@@ -10,7 +10,6 @@ class Array
 		end
 		memo
 	end
-
 
 
 end
