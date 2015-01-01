935169c10d46754b21969009426a11fb58fbc4bd
guspowell <augustuspowell@me.com>
2015-01-01 17:05:51 +0000

    first test passes

--- Array/Inject/lib/inject.rb
+++ Array/Inject/lib/inject.rb
@@ -1,26 +1,16 @@
 class Array
 
-	# .inject {|memo,item| memo + item}
+	# [1,2,3,4].inject {|memo,item| memo + item}
 
-	def my_inject(&block)
-		copy = self.dup
-		copy.each do |item|
-			block.call memo,item #could use &yield as argument and then block.cal here
-			p item
+	def my_inject
+		copy = self.dup					# copy = [1,2,3,4]
+		memo = copy.shift				# memo = 1
+		copy.each do |item|				# going through each of copy
+			memo = yield memo,item 		# my inject { |memo, item| 1 + each item of copy }
 		end
+		memo
 	end
-
-# [1,2,3,4]
-# {|memo,item| memo + item}
-
-
-
-
 
 
 end
 
-
-# list = [{name: 'banana', price: 10}, {name: 'toast', price: 20}]
-
-# list.inject(0){|memo,item| memo + item[:price]}
