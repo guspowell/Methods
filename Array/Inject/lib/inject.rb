dd47a68ee1870e76492dfa0181afe52598dcf832
guspowell <augustuspowell@me.com>
2015-01-05 18:47:22 +0000

    added if_a Symbol statement

--- Array/Inject/lib/inject.rb
+++ Array/Inject/lib/inject.rb
@@ -1,14 +1,24 @@
 class Array
 
-	# [1,2,3,4].inject {|memo,item| memo + item}
+	# [1,2,3,4].inject (:+)
 
 	def my_inject(memo = self[0], sym = nil)
 		copy = self.dup																# copy = [1,2,3,4]
+		p memo
+		if memo.is_a?(Symbol)
+			  copy.shift
+			  copy.each {|item| @memo = @memo.send(symbol, item)}
+		
+
+		else
 		copy.shift if memo == self[0]									# copy = [2,3,4]
 		copy.each do |item|
 			memo = yield memo,item
 		end
 		memo
+		end
+
+
 	end
 
 
